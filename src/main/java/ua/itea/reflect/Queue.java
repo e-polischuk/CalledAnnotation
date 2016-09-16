a02cf629d037b48de3124124aa6ffa0b848d2a41
e-polischuk <z.polischuk@gmail.com>
2016-09-16 13:40:44 +0300

    Update Queue.java

--- src/main/java/ua/itea/reflect/Queue.java
+++ src/main/java/ua/itea/reflect/Queue.java
@@ -10,19 +10,25 @@ public class Queue extends Stack {
 	
 	public Queue(int capacity) {
 		super(capacity);
+		
 	}
 	
 	@Called
 	public Integer pop() {
 		showEntry();
+		
 		if(top >= 0) {
 			Integer element = stack[0];
 			for(int i = 0; i < top; i++) {
 				stack[i] = stack[i + 1];
 			}
 			stack[top--] = null;
+			
 			return element;
-		} else return null;
+			
+		} else
+			return null;
+			
 	}
 
 }
